4bcbaf9028f5d597eecc912d353c948792c1926c
lluis <caminoperez.lluis@gmail.com>
2022-04-30 10:54:28 +0200

    Fetch user data from frontend

--- backend/src/routes/githubUser.js
+++ backend/src/routes/githubUser.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const { URLSearchParams } = require('url');
-require('dotenv').config()
+require('dotenv').config();
 
 CLIENT_ID = process.env.CLIENT_ID;
 CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -10,13 +10,13 @@ CLIENT_SECRET = process.env.CLIENT_SECRET;
 router.get('/authenticate', (req, res) => {
     const params = new URLSearchParams({
         client_id: CLIENT_ID,
-        redirect_uri: 'http://127.0.0.1:8080/githubUser/getInfo'
-    })
+      redirect_uri: 'http://127.0.0.1:3000/redirect'
+    });
     res.status(301).redirect('https://github.com/login/oauth/authorize/?' + params)
 });
 
 router.get('/getInfo', async (req, res) => {
-    const code = req.query.code
+  const code = req.query.code;
     let accessToken;
     try {
         accessToken = await axios({
@@ -32,18 +32,24 @@ router.get('/getInfo', async (req, res) => {
         });
     } catch (err) {
         console.error("Failed to obtain access token");
-        debug(err.stack)
+      debug(err.stack);
         res.sendStatus(500);
     }
 
     const token = accessToken.data.access_token;
 
-    const userInfo = await axios({
-        method: 'get',
-        url: 'https://api.github.com/user',
-        headers: { 'Authorization': 'token ' + token },
-    });
+  if (!token) {
+    console.log("Invalid token");
+    return;
+  }
+  console.log(token);
 
+  try {
+    const userInfo = await axios({
+      method: 'get',
+      url: 'https://api.github.com/user',
+      headers: {'Authorization': 'token ' + token},
+    });
     // call urls and for each, access languages, topics and readme contents
     const starredURL = userInfo.data.starred_url;
     const reposURL = userInfo.data.repos_url;
@@ -57,14 +63,17 @@ router.get('/getInfo', async (req, res) => {
     // console.log("bio " + bio)
 
     const userRepos = await axios({
-        method: 'get',
-        url: reposURL,
-        // headers: { 'Authorization': 'token ' + token },
+      method: 'get',
+      url: reposURL,
+      // headers: { 'Authorization': 'token ' + token },
     });
 
     console.log(userRepos.data)
+  } catch (e) {
+
+  }
 
 
-})
+});
 
 module.exports = router;
--- frontend/src/GithubRedirect.tsx
+++ frontend/src/GithubRedirect.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSearchParams} from "react-router-dom";
 
 function GitHubRedirect() {
     const [searchParams] = useSearchParams();
 
-    console.log(searchParams);
+    console.log(searchParams.get('code'));
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:8080/githubUser/getInfo?code=${searchParams.get('code')}`)
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }, []);
 
     return (<></>);
 }
