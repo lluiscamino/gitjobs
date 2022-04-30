f2a381c263ee136b779ec82842292c09c3cfec9c
lluis <caminoperez.lluis@gmail.com>
2022-04-30 10:07:36 +0200

    Install React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,37 +4,43 @@ import './App.css';
 import {Nav, Navbar, NavDropdown, ThemeProvider} from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import GitForm from './GitForm';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <ThemeProvider
-            breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
-        >
-            <Navbar bg="light" expand="lg">
-                <Container>
-                    <Navbar.Brand href="#home">Travahos</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider/>
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+      <Router>
+          <div className="App">
+              <ThemeProvider
+                  breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+              >
+                  <Navbar bg="light" expand="lg">
+                      <Container>
+                          <Navbar.Brand href="#home">Travahos</Navbar.Brand>
+                          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                          <Navbar.Collapse id="basic-navbar-nav">
+                              <Nav className="me-auto">
+                                  <Nav.Link href="#home">Home</Nav.Link>
+                                  <Nav.Link href="#link">Link</Nav.Link>
+                                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                      <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                      <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                      <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                      <NavDropdown.Divider/>
+                                      <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                  </NavDropdown>
+                              </Nav>
+                          </Navbar.Collapse>
+                      </Container>
+                  </Navbar>
 
-            <GitForm></GitForm>
+                  <Routes>
+                      <Route path="/" element={<GitForm/>}/>
+                      <Route path="/user/:{user}"/>
+                  </Routes>
 
-        </ThemeProvider>
-    </div>
+              </ThemeProvider>
+          </div>
+      </Router>
   );
 }
 
