aea51eac08a7d84f18dbbbf6eeeefbbbd4eb36c7
João Martins <up201707311@fe.up.pt>
2022-05-01 03:35:09 +0100

    Sort repo languages by value

--- backend/src/utils/keywordExtractor.js
+++ backend/src/utils/keywordExtractor.js
@@ -1,10 +1,10 @@
-const {PythonShell} = require('python-shell')
+const { PythonShell } = require('python-shell')
 
 const keywordExtract = (bio, readmes) => {
     return new Promise((res, _) => {
         const bigReadme = readmes.join('')
 
-        PythonShell.run('src/utils/keyword-extractor.py',{args: [bio,bigReadme]}, (err,results) =>{
+        PythonShell.run('src/utils/keyword-extractor.py', { args: [bio, bigReadme] }, (err, results) => {
             if (err) throw err;
             res(JSON.parse(results))
         });
@@ -13,11 +13,14 @@ const keywordExtract = (bio, readmes) => {
 
 const filterKeywords = (extractedKeywords, repoLanguagesSummary) => {
     const languagesKeywords = [...extractedKeywords.programming_languages]
-    const repoLanguages = Object.keys(repoLanguagesSummary)
+    const sortedRepoLanguages = Object.entries(repoLanguagesSummary)
+        .sort(([, a], [, b]) => b - a)
+        .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+    const repoLanguages = Object.keys(sortedRepoLanguages)
     let i = languagesKeywords.length
     let j = 0
-    while(i < 5 && j < repoLanguages.length) {
-        if(!languagesKeywords.includes(repoLanguages[j].toLowerCase())) {
+    while (i < 5 && j < repoLanguages.length) {
+        if (!languagesKeywords.includes(repoLanguages[j].toLowerCase())) {
             languagesKeywords.push(repoLanguages[j])
             i++
         }
@@ -27,4 +30,4 @@ const filterKeywords = (extractedKeywords, repoLanguagesSummary) => {
     return languagesKeywords.concat(extractedKeywords.roles, extractedKeywords.fields)
 }
 
-module.exports = {keywordExtract, filterKeywords};
+module.exports = { keywordExtract, filterKeywords };
